c7630ac48e39fd68139aa5c1ca20b38d6a206eaa
TheCedarPrince <jacobszelko@gmail.com>
2024-03-10 17:00:16 -0400

    Experiments with wilkinson

--- src/algorithms/wilkinson.jl
+++ src/algorithms/wilkinson.jl
@@ -14,47 +14,32 @@ A simple implementation like Matplotlib's algorithm.
 struct WilkinsonBeeswarm <: BeeswarmAlgorithm
 end
 
-function calculate!(buffer::AbstractVector{<: Point2}, alg::WilkinsonBeeswarm, positions::AbstractVector{<: Point2}, markersize::Real)
+function calculate!(buffer::AbstractVector{<: Point2}, alg::WilkinsonBeeswarm, positions::AbstractVector{<: Point2}, markersize::Real, side::Symbol)
     #= 
     
     Initial sketch of code credited to Frederic Freyer and Julius Krumbiegel.
     Reference here: https://github.com/MakieOrg/Makie.jl/issues/1950
 
     =# 
-    @info "Calculating..."
-    using Random
-    Random.seed!(123)
 
-    f = Figure()
-    ax = Axis(f[1, 1])
-
-    xs = 1:15
-    binwidth = step(xs)
-
-    ms = lift(ax.scene.px_area, ax.finallimits) do pxa, lims
-        widths(pxa)[1] / widths(lims)[1] * binwidth
+    new_positions = Float32[]
+    for pos in positions
+        push!(new_positions, pos[2]) 
     end
+    h = fit(Histogram, new_positions; nbins = markersize)
+    xs = 1:length(h.weights)
 
-    dots = lift(ax.scene.px_area, ax.finallimits) do pxa, lims
-        counts = rand(1:7, length(xs))
-        points = Point2f[]
-        for (x, count) in zip(xs, counts)
-            for i in 1:count
-                push!(
-                    points,
-                    Point2f(
-                        (x - lims.origin[1]) / (lims.widths[1]) * pxa.widths[1],
-                        (i - 0.5) * ms[],
-                    )
-                )
-            end
+    dot_per_bin = round.(Int, h.weights ./ markersize)
+
+    points = Point2f[]
+    for x in xs
+        for dot in 1:dot_per_bin[x]
+            point = Point2f(x, dot)
+            push!(points, point)
         end
-        points
     end
+    println(points)
 
-    scatter!(ax, dots, markersize = ms, space = :pixel)
-    xlims!(ax, xs.start - 1, xs.stop + 1)
-    hideydecorations!(ax)
-    # lines(f[1, 2], 1:10) # add other content to figure
-    f
+    return points
+
 end
