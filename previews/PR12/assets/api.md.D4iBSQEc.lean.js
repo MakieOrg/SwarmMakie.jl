ab6b21957e5c5b1cb7c926f6b8a70dc5d51a8261
Documenter.jl <documenter@juliadocs.github.io>
2024-03-21 02:16:47 +0000

    build based on f54789e

---
+++ previews/PR12/assets/api.md.D4iBSQEc.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as i,a7 as s}from"./chunks/framework.D2mlBOy-.js";const b=JSON.parse('{"title":"API Reference","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),r={name:"api.md"},t=s("",26),n=[t];function o(l,p,d,c,h,k){return i(),e("div",null,n)}const u=a(r,[["render",o]]);export{b as __pageData,u as default};
