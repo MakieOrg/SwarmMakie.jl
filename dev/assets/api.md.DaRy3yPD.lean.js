7834e417f52f91d5431f038e01dae468a8de45cc
Documenter.jl <documenter@juliadocs.github.io>
2024-05-11 22:57:17 +0000

    build based on 0aaf4ef

--- dev/assets/api.md.DaRy3yPD.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as i,a6 as s}from"./chunks/framework.DYxXWMUU.js";const m=JSON.parse('{"title":"API Reference","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),r={name:"api.md"},t=s("",26),n=[t];function o(l,p,d,c,h,k){return i(),e("div",null,n)}const u=a(r,[["render",o]]);export{m as __pageData,u as default};
