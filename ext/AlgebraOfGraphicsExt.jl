81f2b86540004c408ba8cb4dac7bc86c1ebf4029
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-09-12 15:36:31 -0700

    Update AlgebraOfGraphicsExt.jl

--- ext/AlgebraOfGraphicsExt.jl
+++ ext/AlgebraOfGraphicsExt.jl
@@ -4,8 +4,8 @@ import AlgebraOfGraphics as AOG
 
 function AlgebraOfGraphics.aesthetic_mapping(::Type{Beeswarm}, ::AOG.Normal, ::AOG.Normal)
     AOG.dictionary([
-        1 => AesX,
-        2 => AesY,
+        1 => AOG.AesX,
+        2 => AOG.AesY,
         :color => AOG.AesColor,
         :marker => AOG.AesMarker,
         :markersize => AOG.AesMarkerSize,
