50a1f413b179d3c1f11fce8428819c7e06671e5d
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-09-12 15:44:48 -0700

    Add a legend elements definition, lifted from scatter

--- ext/AlgebraOfGraphicsExt.jl
+++ ext/AlgebraOfGraphicsExt.jl
@@ -1,5 +1,5 @@
 module AlgebraOfGraphicsExt
-using SwarmMakie, AlgebraOfGraphics
+using SwarmMakie, AlgebraOfGraphics, Makie
 import AlgebraOfGraphics as AOG
 
 function AlgebraOfGraphics.aesthetic_mapping(::Type{Beeswarm}, ::AOG.Normal, ::AOG.Normal)
@@ -10,6 +10,18 @@ function AlgebraOfGraphics.aesthetic_mapping(::Type{Beeswarm}, ::AOG.Normal, ::A
         :marker => AOG.AesMarker,
         :markersize => AOG.AesMarkerSize,
     ])
+end
+
+
+function AOG.legend_elements(T::Type{Beeswarm}, attributes, scale_args::AOG.MixedArguments)
+    [Makie.MarkerElement(
+        color = AOG._get(T, scale_args, attributes, :color),
+        markerpoints = [Point2f(0.5, 0.5)],
+        marker = AOG._get(T, scale_args, attributes, :marker),
+        markerstrokewidth = AOG._get(T, scale_args, attributes, :strokewidth),
+        markersize = AOG._get(T, scale_args, attributes, :markersize),
+        markerstrokecolor = AOG._get(T, scale_args, attributes, :strokecolor),
+    )]
 end
 
 end
